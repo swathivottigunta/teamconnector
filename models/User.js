708a70840b96afb4e65d43e3a825935eca74f1db
TRAINLYNC14 <trainlync14@hq.huskyenergy.com>
2020-05-08 06:52:34 -0600

    register users - user exists, gravatar, bcrypt

--- models/User.js
+++ models/User.js
@@ -14,7 +14,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  avator: {
+  avatar: {
     type: String,
   },
   date: {
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 // @route  POST api/users
 // @desc   register user
@@ -16,12 +20,43 @@ router.post(
       'please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('user route');
+
+    const { name, email, password } = req.body;
+    try {
+      // User exists
+      let isUser = await User.findOne({ email });
+      if (isUser) {
+        return res
+          .status(400)
+          .send({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Get users gravator default size, read, defaultimage user icon
+      const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
+
+      let user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Bcrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      // return jsonWebToken
+      res.send('user registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
