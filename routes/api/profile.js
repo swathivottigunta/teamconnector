87511605719ca99de65d5bd443290a16a4b5f8e5
TRAINLYNC14 <trainlync14@hq.huskyenergy.com>
2020-05-10 16:30:04 -0600

    Creating Profile Model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route  GET api/profile
-// @desc   Test route
+// Modal
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route  GET api/profile/me
+// @desc   Get current users profile
 // @access public
 
-router.get('/', (req, res) => res.send('Profile route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const profile = await (await Profile.findOne({ user: req.user.id })).populate('user',);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+  res.send('Profile route');
+});
 
 module.exports = router;
