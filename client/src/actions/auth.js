b72b55d168adb33aa48847c5ac07ac17cd4f05ba
TRAINLYNC14 <trainlync14@huskyenergy.com>
2020-06-24 17:43:21 -0600

    log out , navbar links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -79,4 +80,9 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// LogOut / clear profile
+export const logout = () => async (dispatch) => {
+  dispatch({ type: LOGOUT });
 };
