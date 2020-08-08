d0ee0ffc8260e5fae91cdb7320b2355d33f50abe
TRAINLYNC14 <trainlync14@huskyenergy.com>
2020-08-08 00:24:44 -0600

    Delete education, experience & account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,6 +9,7 @@ const { check, validationResult } = require('express-validator');
 // Modal
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route  GET api/profile/me
 // @desc   Get current users profile
@@ -151,6 +152,9 @@ router.get('/user/:user_id', async (req, res) => {
 
 router.delete('/', auth, async (req, res) => {
   try {
+    // Remove User Posts
+    await Profile.deleteMany({ user: req.user.id });
+
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
 
