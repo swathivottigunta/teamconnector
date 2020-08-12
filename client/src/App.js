172a32559a415b8a1466961c99d818b4d011469e
TRAINLYNC14 <trainlync14@huskyenergy.com>
2020-08-12 01:28:37 -0600

    Post reducer, actions & Components

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import AddExperience from './components/profile-form/AddExperience';
 import AddEducation from './components/profile-form/AddEduction';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/post/Posts';
 import PrivaeRoute from './components/routing/PrivaeRoute';
 
 //Redux
@@ -61,6 +62,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivaeRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
